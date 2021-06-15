e8381781519b18be92375ef845f62426c0ef78ee
sbk <ssabadosh@unionstmedia.com>
2021-06-15 18:03:23 +0300

    Added exlude words

---
+++ app/Entities/ExcludeWords.php
@@ -0,0 +1,18 @@
+<?php 
+
+namespace App\Entities;
+
+class ExcludeWords {
+    private $words = [];
+    private $excludes = ['a'];
+
+    public function __construct(array $words)
+    {
+        $this->words = $words;
+    }
+
+    public function exclude(): array
+    {
+        return array_diff($this->words, $this->excludes);
+    }
+}
--- app/index.php
+++ app/index.php
@@ -1,11 +1,17 @@
 <?php 
 
 require_once(__DIR__.'/Entities/TextSplitToWords.php');
+require_once(__DIR__.'/Entities/ExcludeWords.php');
 
 use App\Entities\TextSplitToWords;
+use App\Entities\ExcludeWords;
 
 $text = "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).";
 
-$words = new TextSplitToWords($text, 1);
+$textSpliter = new TextSplitToWords($text, 1);
+$words = $textSpliter->splitWords();
 
-print_r($words->splitWords());
+$wordsExcluder = new ExcludeWords($words);
+$words = $wordsExcluder->exclude();
+
+print_r($words);
